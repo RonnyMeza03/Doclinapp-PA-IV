5c32b7ffc392aec8fe88435d7aaa2e412522f712
akadiegos1017 <diegoceto1017@gmail.com>
2024-03-03 15:15:24 -0500

    Agregué el front del Sidebar

---
+++ src/Usuarios.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Usuarios = ({ toggleSidebar }) => {
+    return (
+      <div className="content">
+        <h1>Contenido de Usuarios</h1>
+        <p>Aquí encontrarás la información sobre los usuarios.</p>
+      </div>
+    );
+  };
+  
+  export default Usuarios;
