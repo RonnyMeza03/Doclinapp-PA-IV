7cd87d836b137664ab31bc42f04bfce8c52911d9
Elias <gvaldelamar2003@gmail.com>
2024-04-21 18:52:06 -0500

    Agregando estilos a Dialogo.js

--- client/src/componentes/paginas/function-informes/Dialogo.js
+++ client/src/componentes/paginas/function-informes/Dialogo.js
@@ -5,10 +5,55 @@ const Dialogo = ({ personaSeleccionada, onClose }) => {
   return (
     <div className="dialogo-container">
       <h2>{personaSeleccionada.nombre}</h2>
-      <p className='jump'><strong>Edad:</strong> {personaSeleccionada.edad}</p>
-      <p className='jump'><strong>Dirección:</strong> {personaSeleccionada.direccion}</p>
-      <p className='jump'><strong>Teléfono:</strong> {personaSeleccionada.telefono}</p>
-      <p className='jump'><strong>Email:</strong> {personaSeleccionada.email}</p>
+      
+      <div className="personal-info border">
+        <b>Información Personal:</b>
+        <div className='separation'>
+          <p><strong>Edad:</strong> {personaSeleccionada.edad}</p>
+          <p><strong>Sexo:</strong> ------------------</p>
+          <p><strong>Dirección:</strong> {personaSeleccionada.direccion}</p>
+          <p><strong>Teléfono:</strong> {personaSeleccionada.telefono}</p>
+        </div>
+      </div>
+
+      <div className='medical-history border'>
+        <b>Antecedentes Médicos:</b>
+        <div className='separation'>
+          <p><strong>Presión Arterial Sistólica:</strong> ------------------</p>
+          <p><strong>Presión Arterial Diastólica:</strong> ------------------</p>
+        </div>
+        <p><strong>Niveles de Colesterol Total:</strong> ------------------</p>
+        <div className='separation'>
+          <p><strong>Niveles de LDL (Colesterol Malo):</strong> -----------------</p>
+          <p><strong>Niveles de HDL (Colesterol Bueno):</strong> ------------------</p>
+        </div>
+        <div className='separation'>
+        <p><strong>Niveles de Triglicéridos:</strong> ------------------</p>
+        <p><strong>Historial Familiar de Enfermedades Cardiacas:</strong> ------------------</p>
+        </div>
+        <p><strong>Historial de Enfermedades Crónicas (ej: diabetes):</strong> ------------------</p>
+      </div>
+
+      <div className="life-style border">
+        <b>Estilo de Vida:</b>
+        <div className="separation">
+          <p><strong>Hábito de Fumar (Si/No):</strong> ------------------</p>
+          <p><strong>Frecuencia de Consumo de Alcohol:</strong> ------------------</p>
+        </div>
+        <div className="separation">
+          <p><strong>Dieta (Alta/Baja en Grasas Saturadas y Colesterol):</strong> ------------------</p>
+          <p><strong>Nivel de Actividad Física (Horas a la Semana):</strong> ------------------</p>
+        </div>
+      </div>
+
+      <div className="risk-evaluation border">
+        <b>Evaluación de Riesgo:</b>
+        <div className="separation">
+        <p><strong>Índice de Masa Corporal (IMC):</strong> ------------------</p>
+        <p><strong>Niveles de Glucosa en Sangre:</strong> ------------------</p>
+        </div>
+      </div>
+
       <button onClick={onClose}>Cerrar</button>
     </div>
   );
