ad9396ab67e287b8fe6b914a426c33f6a4e5cc60
DiegoSeña <89817053+akadiegos1017@users.noreply.github.com>
2024-04-29 16:12:48 -0500

    Agregué un boton basico para devolver al Inicio de Sesión

--- client/src/componentes/paginas/Ajustes.js
+++ client/src/componentes/paginas/Ajustes.js
@@ -1,7 +1,18 @@
+import {Link} from 'react-router-dom'
+
 const Ajustes = ( ) => {
+    const cerrarSesion=() =>{
+
+    }
     return (
-        <div>Ajustes</div>
-    )
+        <div>
+          <h1>Ajustes</h1>
+          {/* Agrega un botón que redirija a la página de inicio de sesión */}
+          <Link to="/">
+            <button onClick={cerrarSesion}>Cerrar Sesión</button>
+          </Link>
+        </div>
+      );
 }
 
 export default Ajustes
