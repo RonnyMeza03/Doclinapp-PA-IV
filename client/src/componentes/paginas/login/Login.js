4be6e27ac6185474c9c28e2cd51818ff26fd9d0f
DiegoSeña <89817053+akadiegos1017@users.noreply.github.com>
2024-05-01 20:39:22 -0500

    Se agregó la carpeta Login con Login.js

---
+++ client/src/componentes/paginas/login/Login.js
@@ -0,0 +1,31 @@
+import '../../css/login.css';
+import logo from '../../img/login.png'; 
+import {Link} from 'react-router-dom'
+
+const Login = ({  }) => {
+    return (
+        <div className="login-container">
+            <div className="login">
+                <h1>DoclinAPP</h1>
+                <p>Login DoclinAPP</p>
+                <div>
+                    <input type="text" placeholder="Usuario" />
+                </div>
+                <div>
+                    <input type="password" placeholder="Contraseña" />
+                </div>
+                <a href="#">¿Olvidaste tu Contraseña?</a>
+                <div>
+                    <Link to="/inicio">
+                    <input type="button" value="INICIAR SESIÓN" className="button"/>
+                    </Link>
+                </div>
+            </div>
+            <div className="img">
+                <img src={logo} alt="Logo" /> {/* Mostrar la imagen */}
+            </div>
+        </div>
+    );
+};
+
+export default Login;
