a5b41b9a6b54a4718d3cca1d1b29fd59d4a6665b
Ronny Josue Meza Ascanio <Jjosuemmeza@gmail.com>
2024-10-29 17:26:16 -0500

    Co-authored-by: EzequielPalencia <EzequielPalencia@users.noreply.github.com>

--- client/src/componentes/paginas/funtion-configuracion/Perfil.js
+++ client/src/componentes/paginas/funtion-configuracion/Perfil.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import { obtenerPerfil } from "../../../api/usuarios.api";
+import React, { useState, useEffect, useCallback } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { obtenerPerfil } from '../../../api/usuarios.api';
+import "../../css/Perfil.css";
+import { FaUser, FaEnvelope, FaBirthdayCake, FaPhone, FaUserShield, FaStar } from 'react-icons/fa';
 
-const Perfil = () => {
+const Perfil = ({ dashboardAbierto }) => {
   const { user, isAuthenticated, isLoading } = useAuth0();
-  const [perfil, setPerfil] = useState(null); // Cambiado a null inicialmente
+  const [perfil, setPerfil] = useState(null);
   const [error, setError] = useState(null);
-  const [isLoadingPerfil, setIsLoadingPerfil] = useState(true); // Nuevo estado para carga
+  const [isLoadingPerfil, setIsLoadingPerfil] = useState(true);
 
   const cargarPerfil = useCallback(async () => {
     if (!isAuthenticated || isLoading || !user) {
@@ -41,27 +43,22 @@ const Perfil = () => {
     return <div>No hay datos de usuario disponibles</div>;
   }
 
-  // Verificamos si perfil existe antes de renderizar
-  if (!perfil) {
-    return <div>Cargando datos del perfil...</div>;
-  }
+  const perfilStyle = {
+    width: dashboardAbierto ? 'calc(100% - 250px)' : '100%', // Ajuste dinámico del ancho
+    transition: 'width 0.5s ease', // Transición suave
+  };
 
   return (
-    <div className="perfil-container">
+    <div className="perfil-container" style={perfilStyle}>
       <img src={user.picture} alt={user.name} />
-      <p>{user.name}</p>
-      <p>{user.email}</p>
-      <p>{user.birthdate}</p>
-      <p>{user.phone_number}</p>
-      {/* Datos del perfil */}
-      <p>Rol: {perfil.rol}</p>
-      <p>Premium: {perfil.premium ? "Sí" : "No"}</p>
-      <p>
-        Grupo de trabajo:{" "}
-        {perfil.usuario.nombreAplicacion.nombre === "Invitado"
-          ? "No estas en ningun grupo"
-          : perfil.usuario.nombreAplicacion.nombre}
-      </p>
+      <div className="perfil-datos">
+        <p><FaUser /> <span>{user.name}</span></p>
+        <p><FaEnvelope /> <span>{user.email}</span></p>
+        <p><FaBirthdayCake /> <span>{user.birthdate}</span></p>
+        <p><FaPhone /> <span>{user.phone_number}</span></p>
+        <p><FaUserShield /> <span>Rol: {perfil.rol}</span></p>
+        <p><FaStar /> <span>Premium: {perfil.premium ? 'Sí' : 'No'}</span></p>
+      </div>
     </div>
   );
 };
--- client/src/componentes/paginas/funtion-configuracion/Planes.js
+++ client/src/componentes/paginas/funtion-configuracion/Planes.js
@@ -1,10 +1,63 @@
-import React from 'react'
+import React from "react";
+import "../../css/Planes.css";
+import imgPlanes from "../../imagenes/imgPlanes.jpg";
+import { Link } from "react-router-dom";
 
 const Planes = () => {
   return (
-    <div>Planes</div>
-  )
-}
+    <div>
+      <div className="container">
+        <div className="card">
+          <figure>
+            <img src={imgPlanes} alt="imagen planes" />
+          </figure>
+          <div className="contenido">
+            <h3>Contabilidad</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum
+              minima debitis, excepturi hic dolorum eaque! Eius voluptatem dolor
+              reprehenderit aliquid.
+            </p>
+              <Link to= "/Inicio" className="boton-card">
+                Suscribirse 
+              </Link>
+          </div>
+        </div>
+        <div className="card">
+          <figure>
+            <img src={imgPlanes} alt="imagen planes" />
+          </figure>
+          <div className="contenido">
+            <h3>Administracion</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum
+              minima debitis, excepturi hic dolorum eaque! Eius voluptatem dolor
+              reprehenderit aliquid.
+            </p>
+            <Link to= "/Inicio" className="boton-card">
+                Suscribirse 
+            </Link>
+          </div>
+        </div>
+        <div className="card">
+          <figure>
+            <img src={imgPlanes} alt="imagen planes" />
+          </figure>
+          <div className="contenido">
+            <h3>Ventas</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum
+              minima debitis, excepturi hic dolorum eaque! Eius voluptatem dolor
+              reprehenderit aliquid.
+            </p>
+            <Link to= "/Inicio" className="boton-card">
+              Suscribirse 
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Planes 
-
+export default Planes;
